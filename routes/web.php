323ee05e126030d5cb69a32a65a47f144990a1cb
Paulo Junior <73448096+N7z@users.noreply.github.com>
2025-08-17 19:49:16 -0300

    Adicionadas novas rotas para os CRUDS

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,18 @@ Route::middleware(['auth'])->group(function () {
     Volt::route('clients/create', 'clients/create')->name('clients.create');
     Volt::route('clients/{client}/edit', 'clients/edit')->name('clients.edit');
 
+    Volt::route('categories', 'categories/index')->name('categories.index');
+    Volt::route('categories/create', 'categories/create')->name('categories.create');
+    Volt::route('categories/{category}/edit', 'categories/edit')->name('categories.edit');
+
+    Volt::route('suppliers', 'suppliers/index')->name('suppliers.index');
+    Volt::route('suppliers/create', 'suppliers/create')->name('suppliers.create');
+    Volt::route('suppliers/{supplier}/edit', 'suppliers/edit')->name('suppliers.edit');
+
+    Volt::route('products', 'products/index')->name('products.index');
+    Volt::route('products/create', 'products/create')->name('products.create');
+    Volt::route('products/{product}/edit', 'products/edit')->name('products.edit');
+
     Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
     Volt::route('settings/password', 'settings.password')->name('settings.password');
     Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
